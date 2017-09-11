736925188d2087891071f5f606292d0dbb40394d
TheoRev <hrevillafernandez79@gmail.com>
2017-09-11 00:05:21 -0500

    Consumiendo un api rest

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,11 +1,9 @@
 (function () {
 	angular.module('odontosoft.loginController', [])
 		.controller('LoginController', ['$scope', '$http', function ($scope, $http) {
-			$scope.user = '';
-			$scope.pass = '';
 			$scope.user = {
-				username: $scope.user,
-				password: $scope.pass
+				username: '',
+				password: ''
 			};
 
 			$scope.ingresar = function () {
@@ -14,6 +12,28 @@
 		}]);
 
 	function login($http, $scope) {
-
+		// console.log($scope.user.username);
+		// console.log($scope.user.password);
+		$http({
+				method: 'POST',
+				url: 'http://localhost:3030/api/login',
+				data: {
+					user: $scope.user
+				},
+				headers: 'Content-Type: application/json'
+			})
+			.then(
+				function success(response) {
+					console.log(response);
+				},
+				function error(response) {
+					console.log(response);
+					alert('Error al intentar enviar los datos.');
+				}
+			).catch(
+				function fail(response) {
+					console.log("Excep: " + response);
+				}
+			);
 	}
 })();
